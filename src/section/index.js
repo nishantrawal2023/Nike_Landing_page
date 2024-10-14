fb53df4ae31ab354ef72b6dec4519e9ac964e7cc
nishantrawal2023 <nishantrawal.cs19@gmail.com>
2024-10-14 20:01:51 +0530

    first commit

---
+++ src/section/index.js
@@ -0,0 +1,20 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero"
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+
+
+export{
+    CustomerReviews,
+    Footer,
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    Subscribe,
+}
